28ef2badcccbc927b4c9563b5474ec007a73214c
VonLos <rylandgrounds@gmail.com>
2020-12-27 20:37:22 -0600

    using uuid to create random id's for login testing

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Login from './Login';
 
 function App() {
+  const [id,setId] = useState()
   return (
-    <div className="App">
-     <Login/>
-    </div>
+    <>
+    {id}
+     <Login onIdSubmit={setId}/> 
+    </>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,24 @@
 import React, {useRef} from 'react'
 import {Button, Container, Form} from 'react-bootstrap'
-
-export default function Login() {
+import {v4 as uuidV4} from 'uuid'
+export default function Login({onIdSubmit}) {
     const idRef = useRef()
+    function handleSubmit(e) {
+        e.preventDefault()
+        onIdSubmit(idRef.current.value)
+    }
+    function createNewId() {
+        onIdSubmit(uuidV4())
+    }
     return (
         <Container className="align-items-center d-flex" style={{height: '100vh'}}>
-            <Form className="w-100">
+            <Form onSubmit={handleSubmit} className="w-100">
                 <Form.Group>
                     <Form.Label>Enter ID</Form.Label>
                     <Form.Control type="text" ref={idRef} required /> 
                 </Form.Group>
                 <Button type="submit" className="mr-2">Login</Button>
-                <Button variant="secondary">Generate New Id</Button>
+                <Button onClick={createNewId} variant="secondary">Generate New Id</Button>
                 </Form>
         </Container>
     )
