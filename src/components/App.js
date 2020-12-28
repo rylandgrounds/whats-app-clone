0c89201d8df6589d9e8b3e6e40a4d503956eade1
VonLos <rylandgrounds@gmail.com>
2020-12-28 11:35:32 -0600

    Added contacts feature

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,15 @@ import React from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
+import { ContactsProvider } from "../contexts/ContactsProvider";
 function App() {
   const [id, setId] = useLocalStorage("id");
-  return id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />;
+  const dashboard = (
+  <ContactsProvider>
+       <Dashboard id={id} />
+    </ContactsProvider>
+  )
+  return id ? dashboard: <Login onIdSubmit={setId} />;
 }
 
 export default App;
